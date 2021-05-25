5825aaee14b5dc4aec9726f900c1a90b39ce21b9
maryam22 <maryambar22@gmail.com>
2021-05-25 15:52:47 +0200

    updqte

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-user  = {}
+// document.getElementById("btn").onclick = function(){
+//    document.getElementById("fname").innerHTML = "hello"
+//   //return Math.floor((Math.random() * 10) + 1);
+
+// }
+
+var x = document.getElementById("btn");
+function myFunction(){
+
+var x = document.getElementById("myNumber");
+var randonNumn = Math.floor(Math.random() * 2); 
+ if (x != isNaN){
+ 
+    if(x.value == randonNumn){
+        document.getElementById("demo").innerHTML = "Yes they are equal"
+      
+       } else
+       {
+        document.getElementById("demo").innerHTML ="No they are not equal"
+       }
+
+ }
+
+
+
+};
+
