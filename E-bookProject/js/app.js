5a7b988c3a00c4e21ffa64eb25fe7f1367eb2329
maryam22 <maryambar22@gmail.com>
2021-06-24 13:23:30 +0200

    update

--- E-bookProject/js/app.js
+++ E-bookProject/js/app.js
@@ -28,7 +28,11 @@ function RemoveItems(e) {
     x.addEventListener('click', RemoveItems)
  
  })
- 
+
+
+ let index=0;
+
+
  //Add Element
 
 btnForm.addEventListener('click', addElement)
@@ -37,6 +41,8 @@ btnForm.addEventListener('click', addElement)
     function addElement(e){
     e.preventDefault();
     // make td,tr th
+
+    index++
     let tr = document.createElement("tr");
     let th = document.createElement("th");
     let td1 = document.createElement("td");
@@ -59,23 +65,89 @@ btnForm.addEventListener('click', addElement)
     tr.appendChild(td4)
     tr.appendChild(td5)
     td6.appendChild(template2)
+    td6.childNodes[0].addEventListener('click', RemoveItems)
     tr.appendChild(td6)
+
+
+    
 
     //appanded text
     //let text = document.createTextNode(bookName.value)
-    var t=document.createTextNode("1")
+    var t=document.createTextNode(index)
     th.appendChild(t)
-    td1.appendChild( document.createTextNode(bookName.value))
+    td1.appendChild(document.createTextNode(bookName.value))
     td2.appendChild(document.createTextNode(bookAuthor.value))
     td3.appendChild(document.createTextNode(bookCategory.value))
     td4.appendChild(document.createTextNode(bookDate.value))
     td5.appendChild(document.createTextNode(bookprice.value))
     //print e simbole after
     td5.appendChild(template1)
-    tbody.append(tr)
-
+    let trs={id:index,bookName:bookName.value,bookAuthor:bookAuthor.value,bookCategory:bookCategory.value,bookDate:bookDate.value,bookprice:bookprice.value}
+    tbody.insertBefore(tr,tbody.childNodes[0])
     console.log(tr)
+    addTrs(trs)
+
+}
+
+ //choose categoty
+
+ function chooseOption(number){
+  switch(number){
+  case 1:
+    return "Philoshopy"
+  
+  case 2:
+    return "History"
+  
+  case 3:
+    return "Psychology"
+  
+  case 4:
+    return "Fantasy"
+  
+  case 5: 
+  return "Sci-Fi"
+  
+  case 6:
+    return "Mystery"
+  
+  case 7: 
+  return "Thriller"
+  
+  case 8:
+    return "Romance"
+  
+  case 9:
+    return "Westerns"
+  
+
+  }
+  return "failed"
+}
+
+//getAllTrs
+
+
+function getAllTsFromStorage() {
+    let trs;
+    if (localStorage.getItem('list') === null) {//if trs not exits
+        trs = []
+    }
+    else {
+        trs = JSON.parse(localStorage.getItem('list'))
+    }
+    return trs
 }
 
 
+//add trs to localstorage
 
+function addTrs(newTrs) {
+
+    let temp = getAllTsFromStorage();
+    temp.push(newTrs)
+    console.log(temp)
+    return localStorage.setItem('list', JSON.stringify(temp))
+}
+
+//addTrs(tr)
--- localStorage/app.js
+++ localStorage/app.js
@@ -7,45 +7,70 @@ let myUsers = [{id:1,user:"root"},{id:2,user:"root"}]
 let myFun = ()=>console.log("hello")
 let EBook = [{id:1,user:"root"},{id:2,user:"root"}]
 
+let myNewObject = JSON.stringify(myObject)//string Json
 localStorage.setItem("myString",myString)
 localStorage.setItem("myNumber",myNumber)
 localStorage.setItem("myNumber",logicVar)
-localStorage.setItem("myobject",myObject)
+localStorage.setItem("myobject",myNewObject)
 localStorage.setItem("myUsers",myUsers)
 
-let myNewObject = JSON.stringify(myObject)
-localStorage.setItem("myobject",myObject)
-console.log(typeof myNewObject,typeof myObject)
+//string Json
+// localStorage.setItem("myobject",myObject)
+// console.log(typeof myNewObject,typeof myObject)
 
 
 //add email
 
-//  let myObjectdata =JSON.parse(localStorage.getItem('myObject'))
-//  myObjectdata.email = "vfvdv"
-// // console.log(typeof myObjectdata)
-// // console.log(typeof myObject)
+let myObjectdata = JSON.parse(localStorage.getItem('myobject'))//array object
+myObjectdata.email = "test@test.be"
+console.log("gggg", myObjectdata)
+console.log(typeof myObject)
 
 
 //new scenario
-localStorage.setItem("users",JSON.stringify(myUsers))
-
+localStorage.setItem("users", JSON.stringify(myUsers))//Json string
 let tempUserDB = JSON.parse(localStorage.getItem("users"))//array object
-
-let newUser = {id:3,user:"visitor"}
-
+let newUser = { id: 3, user: "visitor" }
 tempUserDB.push(newUser)
-localStorage.setItem('users',JSON.stringify(tempUserDB))
+localStorage.setItem('users', JSON.stringify(tempUserDB))
 console.log(tempUserDB)
 
 
 //ebook
-localStorage.setItem("EBook",JSON.stringify(EBook))
+localStorage.setItem("EBook", JSON.stringify(EBook))
 let tempUserDB1 = JSON.parse(localStorage.getItem("EBook"))//array object
-let newBook = {id:3,user:"book"}
+let newBook = { id: 3, user: "book" }
 tempUserDB1.push(newBook)
-localStorage.setItem('EBook',JSON.stringify(tempUserDB1))
+localStorage.setItem('EBook', JSON.stringify(tempUserDB1))
 console.log(tempUserDB1)
 
 //get key
 console.log(localStorage.key(5))
 
+//function
+
+
+
+function getAllMoviesFromStorage() {
+    let movies;
+    if (localStorage.getItem('movies') === null) {//if movies not exits
+        movies = []
+    }
+    else {
+        movies = JSON.parse(localStorage.getItem('movies'))
+    }
+    return movies
+}
+
+
+function addMovie(newMovie) {
+
+    let temp = getAllMoviesFromStorage();
+    temp.push(newMovie)
+    console.log(temp)
+    return localStorage.setItem('movies', JSON.stringify(temp))
+}
+
+addMovie({ id: 1, title: "superman" })
+addMovie({ id: 2, title: "batman" })
+localStorage.clear()
